28f41e8c415126459caa429e77bca01b407d9d34
Michael Vorburger <mike@vorburger.ch>
2026-05-07 00:10:30 +0200

    feat: support multiple Markdown files in c function

--- dotfiles/fish/functions/c.fish
+++ dotfiles/fish/functions/c.fish
@@ -1,16 +1,23 @@
 function c --description "Use glow for Markdown files, fallback to bat"
-    set -l target $argv[1]
+    if test (count $argv) -eq 0
+        if command -sq batcat; batcat; else if command -sq bat; bat; else; cat; end
+        return
+    end
 
-    # Check if the first argument ends in .md (case-insensitive)
-    if string match -q -i "*.md" "$target"
-        if command -sq glow
-            glow -p $argv
-            return
+    # Check if all arguments are Markdown files
+    set -l all_md true
+    for arg in $argv
+        if not string match -q -i "*.md" "$arg"
+            set all_md false
+            break
         end
     end
 
-    # Fallbacks for non-markdown files or if glow isn't installed
-    if command -sq batcat
+    if test "$all_md" = true; and command -sq glow
+        for arg in $argv
+            glow -p "$arg"
+        end
+    else if command -sq batcat
         batcat $argv
     else if command -sq bat
         bat $argv
